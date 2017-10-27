e4e17d82c8cb39bbe6f858ad4e3e0c9f8a2a07cf
Sam-PC\Sam <sam.verburg@hotmail.com>
2017-10-27 02:55:53 +0200

    valueconverterlols

---
+++ LeagueOfNinjas/LeagueOfNinjas/ViewModel/ValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using LeagueOfNinjas.ViewModel;
+
+namespace LeagueOfNinjas
+{
+    class ValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (double)value * 1.8000f + 32;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            //De convert back is onnodig. 
+            return value;
+        }
+    }
+}
